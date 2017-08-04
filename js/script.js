6e895de80d9e93239059d15ee2f2e05718cd115f
ctjsctjs <ctjsctjs@gmail.com>
2017-08-05 04:55:29 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,13 +9,13 @@ function readTextFile(file)
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
-				console.log(allText);
-				lineArray = textArea.value.split("\n");
+		console.log(allText);
+		lineArray = textArea.value.split("\n");
 				
-					for(var i=0; i<lineArray.length; i++){
-						console.log(lineArray[i]);
-					}
-				}
+		for(var i=0; i<lineArray.length; i++){
+		console.log(lineArray[i]);
+		}
+	
             }
         }
     }
