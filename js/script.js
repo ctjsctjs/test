5585012592c4010f7243d545398a34c8fbc39f43
ctjsctjs <ctjsctjs@gmail.com>
2017-08-08 01:16:08 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
 $.getJSON("../test/js/adapters.json", function(json) {
 
+	for (var i = 0; i < json.adapters.length; i++){
+	    var obj = json.adapters[i];
+		console.log(obj.name);
+		console.log(obj.image);
+		console.log(obj.link);
+		
+		var div = document.createElement('div');
+
+		var img = document.createElement('img');
+		img.src = obj.image;
+
+		var a = document.createElement('a');
+		a.href = obj.link;
+		a.appendChild(img);
+		a.target = '_blank';
+
+		div.appendChild(a);
+		div.innerHTML += "<div class='item-name black-font'>" + obj.name + "</div>"; 
 
 
-for (var i = 0; i < json.adapters.length; i++){
-    var obj = json.adapters[i];
-	console.log(obj.name);
-	console.log(obj.image);
-	console.log(obj.link);
-}
-
-var div = document.createElement('div');
-
-var img = document.createElement('img');
-img.src = 'images/aftership.png';
-
-var a = document.createElement('a');
-a.href = 'https://www.workato.com/custom_adapters/1316?token=b83769ad';
-a.appendChild(img);
-a.target = '_blank';
-
-div.appendChild(a);
-div.innerHTML += "<div class='item-name black-font'>" + 'test' + "</div>"; 
-
-
-document.getElementById("container-apps").prepend(div);
+		document.getElementById("container-apps").prepend(div);
+	}	
 });
