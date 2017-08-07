cb8f250a09b372b60baec4c8f5f6ec4ab008deb3
ctjsctjs <ctjsctjs@gmail.com>
2017-08-08 01:12:48 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 $.getJSON("../test/js/adapters.json", function(json) {
-	console.log(json.adapters[0].name);
 });
 
+
+for (var i = 0; i < json.adapters.length; i++){
+    var obj = json.adapters[i];
+	console.log(obj.name);
+	console.log(obj.image);
+	console.log(obj.link);
+}
+
+var div = document.createElement('div');
+
+var img = document.createElement('img');
+img.src = 'images/aftership.png';
+
+var a = document.createElement('a');
+a.href = 'https://www.workato.com/custom_adapters/1316?token=b83769ad';
+a.appendChild(img);
+a.target = '_blank';
+
+div.appendChild(a);
+div.innerHTML += "<div class='item-name black-font'>" + 'test' + "</div>"; 
+
+
+document.getElementById("container-apps").prepend(div);
