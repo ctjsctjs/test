6cde989ff173e996cdc973d8e42086ae395da55e
ctjsctjs <ctjsctjs@gmail.com>
2017-08-07 22:50:09 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,25 +1,4 @@
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-		lineArray = allText.value.split("\n");
-				
-		for(var i=0; i<lineArray.length; i++){
-		console.log(lineArray[i]);
-		}
-	
-            }
-        }
-    }
-    rawFile.send(null);
-}
-
-readTextFile("README.md");
+$.getJSON("../js/adapters.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
 
