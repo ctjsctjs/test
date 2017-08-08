103d616d0e0997ef42bea9deb2e3afa577c0b9dc
ctjsctjs <ctjsctjs@gmail.com>
2017-08-09 01:57:39 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
-$.getJSON("../js/adapters.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
-});
+function filterFunc() {
+    // Declare variables
+    var input, filter, a, obj;
+    input = document.getElementById('search-form');
+    filter = input.value.toUpperCase();
+    obj = document.getElementsByClassName('filter');
 
+	for (i=0; i<obj.length; i++){
+		if (obj[i].lastChild.innerHTML.toUpperCase().indexOf(filter)>-1){
+			obj[i].style.display = "";
+		} else {
+			obj[i].style.display = "none";
+		}
+	}
+}
+
+
+$.getJSON("../test/js/adapters.json", function(json) {
+
+	var container = document.getElementById("container-apps");	
+			for (var i = json.adapters.length-1; i >-1; i--){
+				var obj = json.adapters[i];
+				var div = document.createElement('div');
+				div.className = 'filter';
+				var img = document.createElement('img');
+				img.src = obj.image;
+				var a = document.createElement('a');
+				a.href = obj.link;
+				a.appendChild(img);
+				a.target = '_blank';
+				div.appendChild(a);
+				div.innerHTML += "<div class='item-name black-font'>" + obj.name + "</div>"; 
+				container.prepend(div);
+		}
+
+});
