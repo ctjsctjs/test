3aa4554bfd63417e0618cf94433197e094cf175e
ctjsctjs <ctjsctjs@gmail.com>
2017-08-09 02:04:29 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
+function filterFunc() {
+    // Declare variables
+    var input, filter, a, obj;
+    input = document.getElementById('search-form');
+    filter = input.value.toUpperCase();
+    obj = document.getElementsByClassName('filter');
+
+	for (i=0; i<obj.length; i++){
+		if (obj[i].lastChild.innerHTML.toUpperCase().indexOf(filter)>-1){
+			obj[i].style.display = "";
+		} else {
+			obj[i].style.display = "none";
+		}
+	}
+}
+
+
 $.getJSON("../test/js/adapters.json", function(json) {
-	for (var i = json.adapters.length-1; i > -1; i--){
-	    var obj = json.adapters[i];
-		var div = document.createElement('div');
-		var img = document.createElement('img');
-		img.src = obj.image;
-		var a = document.createElement('a');
-		a.href = obj.link;
-		a.appendChild(img);
-		a.target = '_blank';
-		div.appendChild(a);
-		div.innerHTML += "<div class='item-name black-font'>" + obj.name + "</div>"; 
-		document.getElementById("container-apps").prepend(div);
-	}		
+
+	var container = document.getElementById("container-apps");	
+			for (var i = json.adapters.length-1; i >-1; i--){
+				var obj = json.adapters[i];
+				var div = document.createElement('div');
+				div.className = 'filter';
+				var img = document.createElement('img');
+				img.src = obj.image;
+				var a = document.createElement('a');
+				a.href = obj.link;
+				a.appendChild(img);
+				a.target = '_blank';
+				div.appendChild(a);
+				div.innerHTML += "<div class='item-name black-font'>" + obj.name + "</div>"; 
+				container.prepend(div);
+		}
+
 });
