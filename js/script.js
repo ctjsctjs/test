c3284e2975c973bfa1b364e1f61702785676b307
ctjsctjs <ctjsctjs@gmail.com>
2017-08-15 01:46:07 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 //Search Function
 function filterFunc(input) {
-    var userInput = input.value.toUpperCase();
-    var app = $('.app');
-    for (i = 0; i < app.length; i++) {
-        if (app[i].textContent.toUpperCase().indexOf(userInput) > -1) {
-            app[i].style.display = "";
-        } else {
-            app[i].style.display = "none";
-        }
+  var userInput = input.value.toUpperCase();
+  var app = $('.app');
+  for (i = 0; i < app.length; i++) {
+    if (app[i].textContent.toUpperCase().indexOf(userInput) > -1) {
+      app[i].style.display = "";
+    } else {
+      app[i].style.display = "none";
     }
+  }
 }
 
 //Load JSON file to generate app listing
 $.getJSON("../test/adapters/adapters.json", function (json) {
-    var container = $("#container-apps");
-    var listCol = 5;
+  var container = $("#container-apps");
+  var listCol = 5;
 
-    //Generate app list from adapters.JSON
-    json.adapters.forEach(function(adapter) {
-        var li = $('<li class="app">').appendTo(container);
-        var a = $('<a target="_blank">').attr('href', adapter.link).appendTo(li);
-        var img = $('<img>').attr('src', adapter.image).appendTo(a);
-        $('<div class="item-name black-font"></div>').text(adapter.name).appendTo(li);
-    })
+  //Generate app list from adapters.JSON
+  json.adapters.forEach(function (adapter) {
+    var li = $('<li class="app">').appendTo(container);
+    var a = $('<a target="_blank">').attr('href', adapter.link).appendTo(li);
+    var img = $('<img>').attr('src', adapter.image).appendTo(a);
+    $('<div class="item-name black-font"></div>').text(adapter.name).appendTo(li);
+  });
 
-    //Generate dummy box for responsive
-    for (i = 0; i < (listCol); i++) {
-        var li = $('<li class="item flex-dummy"></li>').appendTo(container);
-    }
+  //Generate dummy box for responsive
+  for (i = 0; i < (listCol); i++) {
+    var li = $('<li class="item flex-dummy"></li>').appendTo(container);
+  }
 });
